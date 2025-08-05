ee2d7f60a446c966ba469bd4cd49e5a515c0563d
pablo castillo <pablocastillo@Mac-mini-de-pablo.local>
2025-08-04 22:31:46 -0600

    sigils base done

---
+++ packages/sigils/src/something.ts
@@ -0,0 +1,13 @@
+export const testingThis = (props: { title: string }) => {
+    console.log("Testing this", props);
+    console.log("Testing this", props);
+    console.log("Testing this", props);
+    console.log("Testing this", props);
+    return props;
+}
+
+export const styleVar = {
+    fontFamily: "Inter, sans-serif",
+    fontSize: "16px",
+    primaryColor: "#4A90E2",
+}
